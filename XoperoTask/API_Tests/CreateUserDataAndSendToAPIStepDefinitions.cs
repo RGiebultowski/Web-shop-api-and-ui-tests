6a0355cd6b1cbe21c6ae743d020443ded1e181ce
dxt <raf.giebultowski@gmail.com>
2025-11-07 12:35:06 +0100

    created skeletons of API_Tests

---
+++ XoperoTask/API_Tests/CreateUserDataAndSendToAPIStepDefinitions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XoperoTask.API_Tests
+{
+    public class CreateUserDataAndSendToAPIStepDefinitions
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
---
+++ XoperoTask/API_Tests/GetUserDataFromAPIStepDefinitions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XoperoTask.API_Tests
+{
+    public class GetUserDataFromAPIStepDefinitions
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+    }
+}
